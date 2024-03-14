7c82d7257e036c417ac4514423cd639a4149c50e
hannguyen10 <baohannguyen864@yahoo.de>
2024-03-14 19:22:51 +0100

    test with glb model

--- Marker-based/server.js
+++ Marker-based/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('Marker-based/index');
     
 });
 
