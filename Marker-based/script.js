6b5d9e7651d648fa883161a7d20505c41ffdf9a5
hannguyen10 <baohannguyen864@yahoo.de>
2024-04-01 18:24:00 +0200

    test with pinball image

--- Marker-based/script.js
+++
@@ -1,7 +0,0 @@
-addEventListener("markerFound", function() {
-    document.querySelector("#footer").innerHTML = "marker found";
-});
-
-addEventListener("markerLost", function() {
-    document.querySelector("#footer").innerHTML = "marker lost";
-});
