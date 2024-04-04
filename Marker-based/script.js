58b0b0664609754d714d42e7da87e173da17c067
hannguyen10 <baohannguyen864@yahoo.de>
2024-04-04 18:43:12 +0200

    Update script.js

---
+++ Marker-based/script.js
@@ -0,0 +1,18 @@
+// loadMarker.js
+
+fetch("../Marker-based/test_Hanako")
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Datei nicht gefunden");
+    }
+    return response.text();
+  })
+  .then(data => {
+    // Hier wird die Datei erfolgreich geladen
+    // Setze den Inhalt der Datei als URL für den NFT-Marker
+    var nftMarker = document.querySelector("a-nft[type='nft']");
+    nftMarker.setAttribute("url", data);
+  })
+  .catch(error => {
+    console.error("Fehler beim Laden der Datei:", error);
+  });
